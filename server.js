fbab6527c9317de4b650dcfb5a4c12adb5a81fa9
wande <yewandeadeyemi84@gmail.com>
2023-11-19 13:21:42 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// const { Console } = require('console');
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     if(req.url === '/') {
+//         res.write('welcome to backend development');
+//         res.end();
+//     }
+//     if(req.url === '/api/courses') {
+//         res.write(JSON.stringify({id:1, name: 'cyril', email: 'cycoded@gmail.com', phone: 123456789}));
+//         res.end();
+//     }
+// });
+// server.listen(3000);
+// console.log('listening on port 3000...')
